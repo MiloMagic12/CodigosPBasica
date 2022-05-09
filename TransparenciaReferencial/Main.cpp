faf8695d4154d4eb2f263e159e4aba6e69211bcc
CAMILOARTUROART <>
2022-05-09 17:03:36 +0000

    09/05

---
+++ TransparenciaReferencial/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int a = 1;
+
+int funcion (int numIn){
+  a = a+1;
+  int numFin = numIn * numIn;
+  return numFin;
+}
+int main()
+{
+    int b = 2;
+    int c = 3;
+    cout << "valor de a es: "<< a <<endl; 
+    cout << (a+2*funcion(b))+(c+2*funcion(b))<<endl;
+  cout << "valor de a es: "<< a <<endl;
+  cout << (a+2*funcion(b))+(c+2*funcion(b))<<endl;
+  cout << "valor de a es: "<< a <<endl;
+}
